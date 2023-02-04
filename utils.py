59c78129e4774e4e2c7bced48070bf4dc9a8a63d
geovani <geovanipaz7@gmail.com>
2023-02-04 10:55:33 -0300

    Rest API com autenticação

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas, db_session
+from models import Pessoas, db_session, Usuarios
 
 
 def insere_pessoas():
@@ -21,7 +21,18 @@ def excluir_pessoa():
     pessoa = Pessoas.query.filter_by(nome="geo").first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__=='__main__':
+    insere_usuario('rafael','123')
+    insere_usuario('galleani','321')
+    consulta_todos_usuarios()
     #insere_pessoas()
     #excluir_pessoa()
     consulta_pessoas()
